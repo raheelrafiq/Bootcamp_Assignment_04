8979569a15b920aca7c63a0919964d133ebc9e09
Raheel Rafiq <raheelrafiq44@gmail.com>
2020-06-14 00:53:19 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  let [color, setColor] = useState('White')
+  let [count, setCount] = useState('0')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className={`box white ${color === 'Blue' ? 'blue' : color === 'Green' ? 'green' : color === 'Purple' ? 'purple' : color === 'Red' ? 'red' : 'white'}`}>
+      <h1>Background is : {color}</h1>
+      <button onClick={()=> setColor('Blue')}>Blue</button>
+      <button onClick={()=> setColor('Green')}>Green</button>
+      <button onClick={()=> setColor('Purple')}>Purple</button>
+      <button onClick={()=> setColor('Red')}>Red</button>
+      <h3>Click on Button to change Background Color</h3>
+      </div>
+      <div className='grey box'>
+      <br /><br /><br />
+      <h2>Value of Counter is : {count}</h2>
+      <button onClick={()=> setCount(++count)}>Add</button>
+      <button onClick={()=> setCount(0)}>Reset</button>
+    </div>
     </div>
   );
 }
---
+++ src/Message.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Message(props) {
+    return (
+        <h2>Value of Counter is : {props.counter}</h2>
+    )
+}
